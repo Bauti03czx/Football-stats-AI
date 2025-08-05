c00eb5799f6d2e25c77635bb628a2d1256c8250e
Batrix <Bautistaczx@gmail.com>
2025-08-05 16:03:33 +0200

    Initial try

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Using LLms to filter out football data
 Data used: https://figshare.com/collections/Soccer_match_event_dataset/4415000/2
 
 Pappalardo, Luca; Massucco, Emanuele (2019). Soccer match event dataset. figshare. Collection. https://doi.org/10.6084/m9.figshare.c.4415000
+
+![alt text](image.png)
--- data_handling/try.py
+++ data_handling/try.py
@@ -1,5 +1,12 @@
-
 from kloppy import wyscout
-dataset = wyscout.load("data/wyscout")  # Load from a JSON file
-events  = dataset.to_pandas()                         # plain DataFrame
-events.head()
+
+dataset = wyscout.load(
+    event_data="data/wyscout/events_Italy.json",
+    match_data="data/wyscout/matches_Italy.json",
+    player_data="data/wyscout/players_Italy.json",
+    # Optional arguments
+    event_types=["shot", "pass"],
+    coordinates="wyscout"
+)
+
+dataset.to_df().head()
